bb672cc3963e60cf3df0fb01eb91500e56280ace
relaxcore <yurii.rogulya@teamvoy.com>
2020-02-18 00:45:49 +0200

    [reporting] transaction_id, block number & block date column info

--- apps/reporting/lib/reporting/report/columns/block_time.ex
+++ apps/reporting/lib/reporting/report/columns/block_time.ex
@@ -1,4 +1,5 @@
 defmodule Reporting.Report.Columns.BlockTime do
   def get(transaction) do
+    transaction |> get_in(["block_data", "block_time"])
   end
 end
--- apps/reporting/lib/reporting/report/columns/transaction_id.ex
+++ apps/reporting/lib/reporting/report/columns/transaction_id.ex
@@ -1,4 +1,5 @@
 defmodule Reporting.Report.Columns.TransactionID do
   def get(transaction) do
+    transaction |> get_in(["block_data", "trx_id"])
   end
 end
