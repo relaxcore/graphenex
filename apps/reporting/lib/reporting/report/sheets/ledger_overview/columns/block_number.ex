03e36c33b01ed1ad43dfab0d354623953c7f930a
relaxcore <yurii.rogulya@teamvoy.com>
2020-02-25 22:39:47 +0200

    [reporting] cryptofresh hyperlinks for txid and block_number on ledger overview

--- apps/reporting/lib/reporting/report/sheets/ledger_overview/columns/block_number.ex
+++ apps/reporting/lib/reporting/report/sheets/ledger_overview/columns/block_number.ex
@@ -1,3 +1,9 @@
 defmodule Reporting.Report.Sheets.LedgerOverview.Columns.BlockNumber do
-  def get(transaction), do: transaction |> get_in(["block_data", "block_num"])
+  def get(transaction) do
+    block_number = transaction |> get_in(["block_data", "block_num"])
+    tx_sequence  = transaction |> get_in(["operation_history", "trx_in_block"])
+    dex_url      = "https://wallet.bitshares.org/#/block/#{block_number}/#{tx_sequence}"
+
+    {:formula, "HYPERLINK(\"#{dex_url}\", \"#{block_number}\")"}
+  end
 end
--- apps/reporting/lib/reporting/report/sheets/ledger_overview/columns/transaction_id.ex
+++ apps/reporting/lib/reporting/report/sheets/ledger_overview/columns/transaction_id.ex
@@ -1,3 +1,9 @@
 defmodule Reporting.Report.Sheets.LedgerOverview.Columns.TransactionID do
-  def get(transaction), do: transaction |> get_in(["block_data", "trx_id"])
+  # def get(transaction), do: transaction |> get_in(["block_data", "trx_id"])
+  def get(transaction) do
+    txid            = transaction |> get_in(["block_data", "trx_id"])
+    cryptofresh_url = "https://cryptofresh.com/tx/#{txid}"
+
+    {:formula, "HYPERLINK(\"#{cryptofresh_url}\", \"#{txid}\")"}
+  end
 end
